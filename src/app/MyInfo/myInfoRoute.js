2b8d8db46176383acf042ee6fabb25147d743cae
ryuwldnjs <ryu_eclipse@naver.com>
2023-08-02 06:39:26 +0900

    style : api url 형식 수정

--- src/app/MyInfo/myInfoRoute.js
+++ src/app/MyInfo/myInfoRoute.js
@@ -37,14 +37,14 @@ module.exports = function(app){
 
     //류지원
     // 3. 특정 유저 조회 API
-    app.get('/user/:userId', user.getUserById);
+    app.get('/api/user/:userId', user.getUserById);
     // 특정유저 수정 API
-    app.put('/user/:userId', user.updateUser);
+    app.put('/api/user/:userId', user.updateUser);
 
     // 참여한 대회 리스트 조회API
-    app.get('/user/playList/:userId', user.getPlayListById);
+    app.get('/api/user/playList/:userId', user.getPlayListById);
     // 개최한 대회 리스트 조회API
-    app.get('/user/hostList/:userId', user.getHostListById);
+    app.get('/api/user/hostList/:userId', user.getHostListById);
 
 
 
